652d55e16baff5fec1c23a3cdbd0b579fe6418e8
abuzar.bit <abuzarrehan95@gmail.com>
2022-01-02 01:19:25 +0500

    layout designing

---
+++ app/src/main/java/com/example/conterappbsef18a528/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.conterappbsef18a528;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
